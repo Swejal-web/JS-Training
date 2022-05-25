6f212e530e704108266f9f5ac9ddbf037802f274
Swejal Shrestha <70813833+Swejal-web@users.noreply.github.com>
2022-05-25 09:04:16 +0545

    class added

--- Assignment-4_Collision/js/index.js
+++ Assignment-4_Collision/js/index.js
@@ -1,9 +1,16 @@
 
 const mainDiv = document.getElementById("root");
+//const secondDiv = document.getElementById("second-root");
 
 mainDiv.style.width = 1750 + "px";
 mainDiv.style.height = 900 + "px";
 mainDiv.style.backgroundColor = "grey";
+
+// secondDiv.style.marginTop = 100 + "px";
+// secondDiv.style.top = 1100 + "px";
+// secondDiv.style.width = 1750 + "px";
+// secondDiv.style.height = 900 + "px";
+// secondDiv.style.backgroundColor = "grey";
 
 const maxLength = {
   x: 1750,
@@ -12,7 +19,6 @@ const maxLength = {
 
 let positionX;
 let positionY;
-
 const totalRectangles = 7;
 const maxBoxHeight = 100;
 const maxBoxWidth = 100;
@@ -21,66 +27,83 @@ const minBoxWidth = 10;
 
 const totalBoxes = [];
 
-var box = {};
+class Box {
+  constructor(
+    height,
+    width,
+    positionX,
+    positionY,
+    directionX,
+    directionY,
+    speed,
+    color
+  ) {
+    this.height = height;
+    this.width = width;
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.directionX = directionX;
+    this.directionY = directionY;
+    this.speed = speed;
+    this.color = color;
+    this.divBox = this.drawBox();
+    this.moveBox(totalBoxes);
+  }
 
-const buildBox = (box) => {
-  let { height, width, positionX, positionY, color } = box;
+  drawBox() {
+    const div = document.createElement("div");
+    mainDiv.append(div);
 
-  const div = document.createElement("div");
+    div.style.width = this.height + "px";
+    div.style.height = this.width + "px";
+    div.style.position = "absolute";
+    div.style.backgroundColor = this.color;
+    div.style.left = this.positionX + "px";
+    div.style.top = this.positionY + "px";
+    return div;
+  }
 
-  mainDiv.append(div);
+  moveBox(totalBoxes) {
+    setInterval(() => {
+      this.checkWallCollision();
+      this.checkBoxCollision(totalBoxes);
+      this.divBox.style.left = `${this.positionX}px`;
+      this.divBox.style.top = `${this.positionY}px`;
+    }, 30);
+  }
 
-  div.style.width = height + "px";
-  div.style.height = width + "px";
-  div.style.position = "absolute";
-  div.style.backgroundColor = color;
-  div.style.left = positionX + "px";
-  div.style.top = positionY + "px";
-  return div;
-};
+  checkWallCollision() {
+    this.positionX = this.positionX + this.speed * this.directionX;
+    this.positionY = this.positionY + this.speed * this.directionY;
 
-const checkBoxCollision = (box, recList) => {
-  let storeSpeed;
-  for (let i = 0; i < recList.length; i++) {
-    if (recList[i] === box) continue;
-
-    if (
-      box.positionX + box.width >= recList[i].positionX &&
-      box.positionX <= recList[i].positionX + recList[i].width &&
-      box.positionY + box.height >= recList[i].positionY &&
-      box.positionY <= recList[i].positionY + box.height
-    ) {
-      box.directionX = box.directionX * -1;
-      box.directionY = box.directionY * -1;
-      storeSpeed = box.speed;
-      box.speed = recList[i].speed;
-      recList[i].speed = storeSpeed;
+    if (this.positionX + this.width >= maxLength.x || this.positionX <= 0) {
+      this.directionX = this.directionX * -1;
+    }
+    if (this.positionY + this.height >= maxLength.y || this.positionY <= 0) {
+      this.directionY = this.directionY * -1;
     }
   }
-};
 
-const checkWallCollision = (box) => {
-  box.positionX = box.positionX + box.speed * box.directionX;
+  checkBoxCollision(totalBoxes) {
+    let storeSpeed;
+    for (let i = 0; i < totalBoxes.length; i++) {
+      if (totalBoxes[i] === this) continue;
 
-  box.positionY = box.positionY + box.speed * box.directionY;
-  if (box.positionX + box.width >= maxLength.x || box.positionX <= 0) {
-    box.directionX = box.directionX * -1;
+      if (
+        this.positionX + this.width >= totalBoxes[i].positionX &&
+        this.positionX <= totalBoxes[i].positionX + totalBoxes[i].width &&
+        this.positionY + this.height >= totalBoxes[i].positionY &&
+        this.positionY <= totalBoxes[i].positionY + this.height
+      ) {
+        this.directionX = this.directionX * -1;
+        this.directionY = this.directionY * -1;
+        storeSpeed = this.speed;
+        this.speed = totalBoxes[i].speed;
+        totalBoxes[i].speed = storeSpeed;
+      }
+    }
   }
-  if (box.positionY + box.height >= maxLength.y || box.positionY <= 0) {
-    box.directionY = box.directionY * -1;
-  }
-};
-
-const moveBox = (divBox, box, recList) => {
-  setInterval(function () {
-    checkWallCollision(box);
-
-    checkBoxCollision(box, recList);
-
-    divBox.style.left = `${box.positionX}px`;
-    divBox.style.top = `${box.positionY}px`;
-  }, 30);
-};
+}
 
 function getRandomColor() {
   var letters = "0123456789ABCDEF";
@@ -99,26 +122,125 @@ for (i = 0; i < totalRectangles; i++) {
   positionY =
     Math.floor(Math.random() * (maxLength.y - maxBoxHeight)) + boxHeight;
 
-  box = {
-    height: boxHeight,
-    width: boxWidth,
-    positionX: positionX,
-    positionY: positionY,
-    directionX: Math.random() < 0.5 ? 1 : -1,
-    directionY: Math.random() < 0.5 ? 1 : -1,
-    speed: Math.floor(Math.random() * (15 - 5 + 1)) + 5,
-    color: getRandomColor(),
-  };
+  directionX = Math.random() < 0.5 ? 1 : -1;
+  directionY = Math.random() < 0.5 ? 1 : -1;
+  speed = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
+  color = getRandomColor();
+
+  const box = new Box(
+    boxHeight,
+    boxWidth,
+    positionX,
+    positionY,
+    directionX,
+    directionY,
+    speed,
+    color
+  );
 
   totalBoxes.push(box);
 }
-let innerBox;
 
-function loop() {
-  totalBoxes.forEach((box) => {
-    innerBox = buildBox(box);
-    moveBox(innerBox, box, totalBoxes);
-  });
+const maxLengthBall = {
+  x: 1700,
+  y: 1800,
+};
+
+class Ball {
+  constructor(
+    ballHeight,
+    ballWidth,
+    positionBallX,
+    positionBallY,
+    directionX,
+    directionY,
+    speed,
+    color
+  ) {
+    this.height = ballHeight;
+    this.width = ballWidth;
+    this.positionX = positionBallX;
+    this.positionY = positionBallY;
+    this.directionX = directionX;
+    this.directionY = directionY;
+    this.speed = speed;
+    this.color = color;
+    this.ballDiv = this.drawBall();
+    this.moveBall();
+  }
+
+  drawBall() {
+    const ballDiv = document.createElement("div");
+    secondDiv.append(ballDiv);
+    console.log(secondDiv);
+    ballDiv.style.height = this.height + "px";
+    ballDiv.style.width = this.width + "px";
+    ballDiv.style.borderRadius = "50%";
+    ballDiv.style.position = "absolute";
+    ballDiv.style.backgroundColor = this.color;
+    ballDiv.style.left = this.positionX + "px";
+    ballDiv.style.top = this.positionY + "px";
+    return ballDiv;
+  }
+
+  moveBall() {
+    setInterval(() => {
+      this.checkWallCollision();
+      //this.checkBallCollision(totalBoxes);
+      // this.divBox.style.left = `${this.positionX}px`;
+      // this.divBox.style.top = `${this.positionY}px`;
+
+      this.ballDiv.style.top = `${this.positionY}px`;
+      this.ballDiv.style.left = `${this.positionX}px`;
+    }, 30);
+  }
+
+  checkWallCollision() {
+    this.positionX = this.positionX + this.speed * this.directionX;
+    this.positionY = this.positionY + this.speed * this.directionY;
+    if (this.positionX + this.width >= maxLength.x || this.positionX <= 0) {
+      this.directionX = this.directionX * -1;
+    }
+    if (this.positionY + this.height >= maxLength.y || this.positionY <= 0) {
+      this.directionY = this.directionY * -1;
+    }
+  }
 }
 
-loop();
+// let positionBallX;
+// let positionBallY;
+// const totalBall = 7;
+// const maxBallRadius = 100;
+// const minBallRadius = 10;
+
+// const totalBallsArray = [];
+
+// for (i = 0; i < totalBall; i++) {
+//   const ballHeight = 50;
+//   const ballWidth = 50;
+//   positionBallX =
+//     Math.floor(Math.random() * (maxLengthBall.x - ballWidth * 2)) +
+//     ballWidth * 2;
+//   positionBallY = Math.floor(Math.random() * (maxLengthBall.y - 1200) + 1200);
+
+//   directionX = Math.random() < 0.5 ? 1 : -1;
+//   directionY = Math.random() < 0.5 ? 1 : -1;
+//   speed = Math.floor(Math.random() * (15 - 5 + 1)) + 5;
+//   color = getRandomColor();
+
+//   const ball = new Ball(
+//     ballHeight,
+//     ballWidth,
+//     positionBallX,
+//     positionBallY,
+//     directionX,
+//     directionY,
+//     speed,
+//     color
+//   );
+
+//   totalBallsArray.push(ball);
+// }
+// console.log(totalBallsArray);
+
+
